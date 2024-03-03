bc8546f6190a7981576c1451884633648595ebb4
AgnieszkaAureliaMarczak <agnieszka.aurelia.marczak@gmail.com>
2024-03-03 23:24:22 +0100

    List interface methods

--- src/Overview.java
+++ src/Overview.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.TreeSet;
+
 public class Overview {
     public static void main(String[] args) {
+        Collection<String> list = new TreeSet<>();
 
+        String[] names = {"Anna", "Bob", "Carol", "David", "Edna"};
+        list.addAll(Arrays.asList(names));
+        System.out.println(list);
+
+        list.add("Fred");
+        list.addAll(Arrays.asList("George", "Gary", "Grace"));
+        System.out.println(list);
+        System.out.println("Gary is in the list? " + list.contains("Gary"));
+
+        list.removeIf(s -> s.charAt(0) == 'G');
+        System.out.println(list);
+        System.out.println("Gary is in the list? " + list.contains("Gary"));
     }
 }
