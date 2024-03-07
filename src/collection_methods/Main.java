f7ee5d774efeb4817a0cbf09a643af6c6758df1a
AgnieszkaAureliaMarczak <agnieszka.aurelia.marczak@gmail.com>
2024-03-07 20:09:37 +0100

    Deck of Cards set-up

---
+++ src/collection_methods/Main.java
@@ -0,0 +1,10 @@
+package collection_methods;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
+    }
+}
