a6dee26a135a5f8003fdf868154dc02097feca6e
AgnieszkaAureliaMarczak <agnieszka.aurelia.marczak@gmail.com>
2024-03-09 22:12:49 +0100

    Collections class binarySearch, replaceAll, frequency, min, max, comparing, sort, rotate and swap methods

--- src/collection_methods/Main.java
+++ src/collection_methods/Main.java
@@ -40,16 +40,16 @@ public class Main {
         Card.printDeck(deck, "Reversed Deck of Cards: ", 4);
 
         var sortingAlgorithm = Comparator.comparing(Card::rank).thenComparing(Card::suit);
-        Collections.sort(deck,sortingAlgorithm);
+        Collections.sort(deck, sortingAlgorithm);
         Card.printDeck(deck, "Standard Deck sored by rank, suit", 13);
 
         Collections.reverse(deck);
         Card.printDeck(deck, "Sorted by rank, suit reversed: ", 13);
 
-        List<Card> kings = new ArrayList<>(deck.subList(4,8));
+        List<Card> kings = new ArrayList<>(deck.subList(4, 8));
         Card.printDeck(kings, "Kings in deck", 1);
 
-        List<Card> tens = new ArrayList<>(deck.subList(16,20));
+        List<Card> tens = new ArrayList<>(deck.subList(16, 20));
         Card.printDeck(tens, "Tens in deck", 1);
 
         int subListIndex = Collections.indexOfSubList(deck, tens);
@@ -61,5 +61,54 @@ public class Main {
 
         boolean disjoint2 = Collections.disjoint(kings, tens);
         System.out.println("disjoint = " + disjoint2);
+
+        deck.sort(sortingAlgorithm);
+        Card tenOfHearts = Card.getNumericCard(Card.Suit.HEART, 10);
+        int foundIndex = Collections.binarySearch(deck, tenOfHearts, sortingAlgorithm);
+        System.out.println("foundIndex = " + foundIndex);
+        System.out.println("foundIndex = " + deck.indexOf(tenOfHearts));
+        System.out.println(deck.get(foundIndex));
+
+        Card tenOfClubs = Card.getNumericCard(Card.Suit.CLUB, 10);
+
+        Collections.replaceAll(deck, tenOfClubs, tenOfHearts);
+        Card.printDeck(deck.subList(32, 36), "Tens row", 1);
+
+        Collections.replaceAll(deck, tenOfHearts, tenOfClubs);
+        Card.printDeck(deck.subList(32, 36), "Tens row", 1);
+
+        if (Collections.replaceAll(deck, tenOfHearts, tenOfClubs)) {
+            System.out.println("Tens of hearts replaced with tens of clubs");
+        } else {
+            System.out.println("No tens of hearts found in the list");
+        }
+
+        System.out.println("Ten of Clubs Cards = " + Collections.frequency(deck, tenOfClubs));
+
+        System.out.println("Best Card = " + Collections.max(deck, sortingAlgorithm));
+        System.out.println("Worst Card = " + Collections.min(deck, sortingAlgorithm));
+
+        var sortBySuit = Comparator.comparing(Card::suit).thenComparing(Card::rank);
+        deck.sort(sortBySuit);
+        Card.printDeck(deck, "Sorted by suit, Rank", 4);
+
+        List<Card> copied = new ArrayList<>(deck.subList(0, 13));
+        Collections.rotate(copied, 2);
+        System.out.println("UnRotated: " + deck.subList(0,13));
+        System.out.println("Rotated " + 2 + ": " + copied);
+
+        copied = new ArrayList<>(deck.subList(0, 13));
+        Collections.rotate(copied, -2);
+        System.out.println("UnRotated: " + deck.subList(0,13));
+        System.out.println("Rotated " + -2 + ": " + copied);
+
+        copied = new ArrayList<>(deck.subList(0, 13));
+        for (int i = 0; i < copied.size()/2; i++) {
+            Collections.swap(copied, i,copied.size() - 1 - i);
+        }
+
+        copied = new ArrayList<>(deck.subList(0, 13));
+        Collections.reverse(copied);
+        System.out.println("Using reverse: " + copied);
     }
 }
